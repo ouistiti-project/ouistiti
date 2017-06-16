136e78eae76bb5559cabcf9dd96b13d153f4021c
mchalain <marc.chalain@gmail.com>
2017-06-16 18:55:59 +0200

    [websocket] merge with ouistiti_ws modification

--- utils/websocket.mk
+++ utils/websocket.mk
@@ -8,7 +8,9 @@ ouistiti_ws_CFLAGS-$(DEBUG)+=-g -DDEBUG
 
 bin-$(WS_ECHO)=websocket_echo
 websocket_echo_SOURCES+=websocket/echo.c
-websocket_echo_CFLAGS-$(WEBSOCKET)+=-DSOCKDOMAIN="(AF_MAX+0X100)" -DPTHREAD
+websocket_echo_CFLAGS-$(WEBSOCKET)+=-DPTHREAD
+websocket_echo_CFLAGS-$(WEBSOCKET)+=-DSOCKDOMAIN="(AF_MAX+0X100)"
+websocket_echo_CFLAGS-$(WEBSOCKET)+=-DSOCKPROTOCOL=153
 websocket_echo_LDFLAGS-$(WEBSOCKET)+=-nodefaultlibs
 websocket_echo_LDFLAGS-$(WEBSOCKET)+=-L../libhttpserver/src/httpserver
 websocket_echo_LIBS-$(WEBSOCKET)+=websocket ouistiti_ws c pthread
