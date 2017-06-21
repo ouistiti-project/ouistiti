7cd01d72df2813d92b28dfeff5ffaa7e650df12e
mchalain <marc.chalain@gmail.com>
2017-06-21 19:14:25 +0200

    [websocket] build websocket

--- utils/websocket.mk
+++ utils/websocket.mk
@@ -1,4 +1,5 @@
-lib-$(WEBSOCKET)=ouistiti_ws
+WS_SRC:=websocket/
+lib-$(WEBSOCKET)+=ouistiti_ws
 ouistiti_ws_SOURCES+=websocket.c
 ouistiti_ws_LIBS+=dl
 ouistiti_ws_LIBS+=websocket
@@ -6,24 +7,24 @@ ouistiti_ws_CFLAGS+=-I../libhttpserver/include
 ouistiti_ws_DFLAGS+=-L../libhttpserver/src/httpserver
 ouistiti_ws_CFLAGS-$(DEBUG)+=-g -DDEBUG
 
-bin-$(WS_ECHO)=websocket_echo
-websocket_echo_SOURCES+=websocket/echo.c
+bin-$(WS_ECHO)+=websocket_echo
+websocket_echo_SOURCES+=$(WS_SRC)echo.c
 websocket_echo_CFLAGS-$(WEBSOCKET)+=-DPTHREAD
 websocket_echo_CFLAGS-$(WEBSOCKET)+=-DSOCKDOMAIN="(AF_MAX+0X100)"
 websocket_echo_CFLAGS-$(WEBSOCKET)+=-DSOCKPROTOCOL=153
 websocket_echo_LDFLAGS-$(WEBSOCKET)+=-nodefaultlibs
 websocket_echo_LDFLAGS-$(WEBSOCKET)+=-L../libhttpserver/src/httpserver
-websocket_echo_LIBS-$(WEBSOCKET)+=websocket ouistiti_ws c pthread
+websocket_echo_LIBS-$(WEBSOCKET)+=ouistiti_ws websocket c pthread
 
 websocket_echo_CFLAGS-$(DEBUG)+=-g -DDEBUG
 
-bin-$(WS_CHAT)=websocket_chat
-websocket_chat_SOURCES+=websocket/chat.c
+bin-$(WS_CHAT)+=websocket_chat
+websocket_chat_SOURCES+=$(WS_SRC)chat.c
 websocket_chat_CFLAGS-$(WEBSOCKET)+=-DPTHREAD
 websocket_chat_CFLAGS-$(WEBSOCKET)+=-DSOCKDOMAIN="(AF_MAX+0X100)"
 websocket_chat_CFLAGS-$(WEBSOCKET)+=-DSOCKPROTOCOL=153
 websocket_chat_LDFLAGS-$(WEBSOCKET)+=-nodefaultlibs
 websocket_chat_LDFLAGS-$(WEBSOCKET)+=-L../libhttpserver/src/httpserver
-websocket_chat_LIBS-$(WEBSOCKET)+=websocket ouistiti_ws c pthread
+websocket_chat_LIBS-$(WEBSOCKET)+=ouistiti_ws websocket c pthread
 
 websocket_chat_CFLAGS-$(DEBUG)+=-g -DDEBUG
