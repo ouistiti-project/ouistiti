f88c2ef173a2856721c26a2dd3c5827c67355c16
mchalain <marc.chalain@gmail.com>
2017-07-31 18:50:10 +0200

    [utils] ws syslogd server adds trace tools

--- utils/websocket/syslogd.c
+++ utils/websocket/syslogd.c
@@ -36,6 +36,14 @@
 #include <arpa/inet.h>
 #include <sched.h>
 
+#define err(format, ...) fprintf(stderr, "\x1B[31m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#define warn(format, ...) fprintf(stderr, "\x1B[35m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#ifdef DEBUG
+#define dbg(format, ...) fprintf(stderr, "\x1B[32m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
 typedef struct user_s user_t;
 struct user_s
 {
