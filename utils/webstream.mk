9cb2e661f508919464079acd47020d4bd5648ca8
mchalain <marc.chalain@gmail.com>
2018-02-14 19:22:14 +0100

    [webstream] UDP stream gateway

--- utils/webstream.mk
+++ utils/webstream.mk
@@ -3,3 +3,8 @@ bin-$(DUMMYSTREAM)+=streamer
 streamer_SOURCES+=$(WS_DIR)streamer.c utils.c
 streamer_LIBS+=pthread
 streamer_CFLAGS-$(DEBUG)+=-g -DDEBUG
+
+bin-$(UDPGW)+=udpgw
+udpgw_SOURCES+=$(WS_DIR)udpgw.c utils.c
+udpgw_LIBS+=pthread
+udpgw_CFLAGS-$(DEBUG)+=-g -DDEBUG
