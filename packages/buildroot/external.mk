dab03a33a2deda9edd5efe89627194d3ac94fd6f
mchalain <marc.chalain@gmail.com>
2018-06-05 20:54:50 +0200

    [package] buildroot rules

--- packages/buildroot/external.mk
+++ packages/buildroot/external.mk
@@ -4,6 +4,6 @@
 #
 ################################################################################
 
-include $(sort $(wildcard $(BR2_EXTERNAL_OUISTITI_PATH)/package/ouistiti/libhttpserver/libhttpserver.mk))
-include $(sort $(wildcard $(BR2_EXTERNAL_OUISTITI_PATH)/package/ouistiti/ouistiti/ouistiti.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_OUISTITI_PATH)/libhttpserver/libhttpserver.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_OUISTITI_PATH)/ouistiti/ouistiti.mk))
 
--- packages/buildroot/ouistiti/ouistiti.mk
+++ packages/buildroot/ouistiti/ouistiti.mk
@@ -15,18 +15,20 @@ OUISTITI_LICENSE_FILES = LICENSE
 OUISTITI_DEPENDENCIES += libhttpserver
 OUISTITI_DEPENDENCIES += libconfig
 
+OUISTITI_USERS += www-data
+
 OUISTITI_KCONFIG_FILE = ouistiti.config
 
 OUISTITI_CONF_OPTS = \
 	--prefix=/usr \
 	--sysconfdir=/etc/ouistiti \
-	--host=$(TARGET_CC:%gcc=%) \
 	--enable-static \
 	--disable-dynamic \
 	--disable-websocket-rt \
-	--with-vthread-type=fork
+	--with-vthread-type=fork \
+	--with-libhttpserver-dir=$(BUILD_DIR)/libhttpserver-$(LIBHTTPSERVER_VERSION)
 
-TARGET_MAKE_ENV+=LD=$(TARGET_CC) sysroot=$(STAGING_DIR)
+OUISTITI_MAKE_OPTS+=LD=$(TARGET_CC) sysroot=$(STAGING_DIR)
 
 ifeq ($(BR2_PACKAGE_LIBHTTPSERVER_MBEDTLS),y)
 OUISTITI_DEPENDENCIES += mbedtls
@@ -35,48 +37,48 @@ else
 OUISTITI_CONF_OPTS += --disable-mbedtls
 endif
 
+ifeq ($(BR2_PACKAGE_LIBHTTPSERVER_WEBSOCKET),y)
+OUISTITI_CONF_OPTS += --enable-websocket
+else
+OUISTITI_CONF_OPTS += --disable-websocket
+endif
+
+ifeq ($(BR2_PACKAGE_OUISTITI_AUTH),y)
+OUISTITI_CONF_OPTS += --enable-auth
+else
+OUISTITI_CONF_OPTS += --disable-auth
+endif
+
+ifeq ($(BR2_PACKAGE_OUISTITI_AUTH_SQLITE),y)
+OUISTITI_DEPENDENCIES += sqlite
+OUISTITI_CONF_OPTS += --enable-authz-sqlite
+else
+OUISTITI_CONF_OPTS += --disable-authz-sqlite
+endif
+
+ifeq ($(BR2_PACKAGE_OUISTITI_WS_JSONRPC),y)
+OUISTITI_CONF_OPTS += --enable-ws-jsonrpc
+else
+OUISTITI_CONF_OPTS += --disable-ws-jsonrpc
+endif
+
+ifeq ($(BR2_PACKAGE_OUISTITI_WS_CHAT),y)
+OUISTITI_CONF_OPTS += --enable-ws-chat
+else
+OUISTITI_CONF_OPTS += --disable-ws-chat
+endif
+
+#OUISTITI_MAKE_OPTS+=DEBUG=y
+
 define OUISTITI_CONFIGURE_CMDS
 	cd $(@D); ./configure $(OUISTITI_CONF_OPTS)
 endef
 
-ifeq ($(BR2_PACKAGE_LIBHTTPSERVER_WEBSOCKET),y)
-LIBHTTPSERVER_CONF_OPTS += --enable-websocket
-else
-LIBHTTPSERVER_CONF_OPTS += --disable-websocket
-endif
-
-ifeq ($(BR2_PACKAGE_OUISTITI_AUTH),y)
-LIBHTTPSERVER_CONF_OPTS += --enable-auth
-else
-LIBHTTPSERVER_CONF_OPTS += --disable-auth
-endif
-
-ifeq ($(BR2_PACKAGE_OUISTITI_AUTH_SQLITE),y)
-LIBHTTPSERVER_CONF_OPTS += --enable-auth-sqlite
-else
-LIBHTTPSERVER_CONF_OPTS += --disable-auth-sqlite
-endif
-
-ifeq ($(BR2_PACKAGE_OUISTITI_WS_JSONRPC),y)
-LIBHTTPSERVER_CONF_OPTS += --enable-ws-jsonrpc
-else
-LIBHTTPSERVER_CONF_OPTS += --disable-ws-jsonrpc
-endif
-
-ifeq ($(BR2_PACKAGE_OUISTITI_WS_CHAT),y)
-LIBHTTPSERVER_CONF_OPTS += --enable-ws-chat
-else
-LIBHTTPSERVER_CONF_OPTS += --disable-ws-chat
-endif
-
-#OUISTITI_MAKE_OPTS+=DEBUG=y
-#OUISTITI_MAKE_OPTS+=VTHREAD_TYPE=pthread
-
 define OUISTITI_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) \
 		$(MAKE1) -C $(@D) $(OUISTITI_MAKE_OPTS) clean
-	$(TARGET_CONFIGURE_OPTS) V=1 $(TARGET_MAKE_ENV) \
-		$(MAKE1) -C $(@D) $(OUISTITI_MAKE_OPTS)
+	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) \
+		$(MAKE1) -C $(@D) $(OUISTITI_MAKE_OPTS) V=1
 endef
 
 define OUISTITI_INSTALL_TARGET_CMDS
@@ -84,8 +86,8 @@ define OUISTITI_INSTALL_TARGET_CMDS
 		$(MAKE1) -C $(@D) $(OUISTITI_MAKE_OPTS) \
 		DESTDIR=$(TARGET_DIR) install
 	$(INSTALL) -d -m 0775 $(TARGET_DIR)/var/run/websocket
-	$(INSTALL) -D -m 0755 $(@D)/packages/buildroot/S50ouistiti \
-		$(TARGET_DIR)/etc/init.d/S50ouistiti
+	$(INSTALL) -D -m 0755 $(OUISTITI_PKGDIR)/ouistiti.conf \
+		$(TARGET_DIR)/etc/ouistiti/ouistiti.conf
 endef
 
 define OUISTITI_INSTALL_STAGING_CMDS
@@ -95,8 +97,18 @@ define OUISTITI_INSTALL_STAGING_CMDS
 endef
 
 define OUISTITI_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(@D)/packages/buildroot/S50ouistiti \
+	$(INSTALL) -D -m 0755 $(OUISTITI_PKGDIR)/S50ouistiti \
 		$(TARGET_DIR)/etc/init.d/S50ouistiti
+endef
+
+define OUISTITI_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 $(OUISTITI_PKGDIR)/ouistiti.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/ouistiti.service
+
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+
+	ln -fs ../../../../usr/lib/systemd/system/ouistiti.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/ouistiti.service
 endef
 
 
