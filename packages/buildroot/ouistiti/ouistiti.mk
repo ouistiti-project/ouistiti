5eb994ff2cd80a29b252f8bd50f5dbc18102480c
Marc Chalain <marc.chalain@smile.fr>
2018-08-02 18:27:12 +0200

    [package] rework buildroot rules to be use as autotools

--- packages/buildroot/ouistiti/ouistiti.mk
+++ packages/buildroot/ouistiti/ouistiti.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-#OUISTITI_VERSION = 1.0
+#OUISTITI_VERSION = 2.0
 #OUISTITI_SOURCE = v$(OUISTITI_VERSION).tar.gz
 #OUISTITI_SITE = https://github.com/ouistiti-project/ouistiti/archive
 OUISTITI_VERSION = HEAD
@@ -14,103 +14,99 @@ OUISTITI_LICENSE = MIT
 OUISTITI_LICENSE_FILES = LICENSE
 OUISTITI_DEPENDENCIES += libhttpserver
 OUISTITI_DEPENDENCIES += libconfig
+OUISTITI_MAKE=$(MAKE1)
 
 OUISTITI_USERS += www-data
 
-OUISTITI_KCONFIG_FILE = ouistiti.config
+OUISTITI_KCONFIG_FILE = $(call qstrip,$(OUISTITI_PKGDIR)/config)
+OUISTITI_KCONFIG_OPTS = \
+	CROSS_COMPILE="$(TARGET_CROSS)" \
+	ARCH=$(KERNEL_ARCH) \
+	DESTDIR="$(TARGET_DIR)" \
 
 OUISTITI_CONF_OPTS = \
+	--sysroot=$(STAGING_DIR) \
 	--prefix=/usr \
 	--sysconfdir=/etc/ouistiti \
 	--enable-static \
-	--disable-dynamic \
+	--disable-shared \
+	--disable-error \
+	--disable-check \
 	--disable-websocket-rt \
 	--with-vthread-type=fork \
 	--with-libhttpserver-dir=$(BUILD_DIR)/libhttpserver-$(LIBHTTPSERVER_VERSION)
 
-OUISTITI_MAKE_OPTS+=LD=$(TARGET_CC) sysroot=$(STAGING_DIR)
+OUISTITI_MAKE_OPTS+=V=1
 
 ifeq ($(BR2_PACKAGE_LIBHTTPSERVER_MBEDTLS),y)
 OUISTITI_DEPENDENCIES += mbedtls
 OUISTITI_CONF_OPTS += --enable-mbedtls
+OUISTITI_KCONFIG_OPTS+=MBEDTLS=y
 else
 OUISTITI_CONF_OPTS += --disable-mbedtls
+OUISTITI_KCONFIG_OPTS+=MBEDTLS=n
 endif
 
 ifeq ($(BR2_PACKAGE_LIBHTTPSERVER_WEBSOCKET),y)
 OUISTITI_CONF_OPTS += --enable-websocket
+OUISTITI_KCONFIG_OPTS+=WEBDSOCKET=y
 else
 OUISTITI_CONF_OPTS += --disable-websocket
+OUISTITI_KCONFIG_OPTS+=WEBDSOCKET=n
 endif
 
 ifeq ($(BR2_PACKAGE_OUISTITI_AUTH),y)
 OUISTITI_CONF_OPTS += --enable-auth
+OUISTITI_KCONFIG_OPTS+=AUTH=y
 else
 OUISTITI_CONF_OPTS += --disable-auth
+OUISTITI_KCONFIG_OPTS+=AUTH=n
 endif
 
 ifeq ($(BR2_PACKAGE_OUISTITI_AUTH_SQLITE),y)
 OUISTITI_DEPENDENCIES += sqlite
 OUISTITI_CONF_OPTS += --enable-authz-sqlite
+OUISTITI_KCONFIG_OPTS+=AUTH_SQLITE=y
 else
 OUISTITI_CONF_OPTS += --disable-authz-sqlite
+OUISTITI_KCONFIG_OPTS+=AUTH_SQLITE=n
 endif
 
 ifeq ($(BR2_PACKAGE_OUISTITI_WS_JSONRPC),y)
 OUISTITI_CONF_OPTS += --enable-ws-jsonrpc
+OUISTITI_KCONFIG_OPTS+=WS_JSONRPC=y
 else
 OUISTITI_CONF_OPTS += --disable-ws-jsonrpc
+OUISTITI_KCONFIG_OPTS+=WS_JSONRPC=n
 endif
 
 ifeq ($(BR2_PACKAGE_OUISTITI_WS_CHAT),y)
 OUISTITI_CONF_OPTS += --enable-ws-chat
+OUISTITI_KCONFIG_OPTS+=WS_CHAT=y
 else
 OUISTITI_CONF_OPTS += --disable-ws-chat
+OUISTITI_KCONFIG_OPTS+=WS_CHAT=n
 endif
 
 #OUISTITI_MAKE_OPTS+=DEBUG=y
 
-define OUISTITI_CONFIGURE_CMDS
-	cd $(@D); ./configure $(OUISTITI_CONF_OPTS)
-endef
-
-define OUISTITI_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) \
-		$(MAKE1) -C $(@D) $(OUISTITI_MAKE_OPTS) clean
-	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) \
-		$(MAKE1) -C $(@D) $(OUISTITI_MAKE_OPTS) V=1
-endef
-
 define OUISTITI_INSTALL_TARGET_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) \
-		$(MAKE1) -C $(@D) $(OUISTITI_MAKE_OPTS) \
-		DESTDIR=$(TARGET_DIR) install
-	$(INSTALL) -d -m 0775 $(TARGET_DIR)/var/run/websocket
-	$(INSTALL) -D -m 0755 $(OUISTITI_PKGDIR)/ouistiti.conf \
+	$(INSTALL) -D -m 0644 $(OUISTITI_PKGDIR)/ouistiti.conf \
 		$(TARGET_DIR)/etc/ouistiti/ouistiti.conf
 endef
 
-define OUISTITI_INSTALL_STAGING_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) \
-		$(MAKE1) -C $(@D) $(OUISTITI_MAKE_OPTS) \
-		DESTDIR=$(STAGING_DIR) install
-endef
-
-define OUISTITI_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(OUISTITI_PKGDIR)/S50ouistiti \
-		$(TARGET_DIR)/etc/init.d/S50ouistiti
-endef
-
 define OUISTITI_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 0644 $(OUISTITI_PKGDIR)/ouistiti.service \
+	$(INSTALL) -D -m 644 $(OUISTITI_PKGDIR)/ouistiti.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/ouistiti.service
-
 	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
-
 	ln -fs ../../../../usr/lib/systemd/system/ouistiti.service \
 		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/ouistiti.service
 endef
+define OUISTITI_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 $(OUISTITI_PKGDIR)/S50ouistiti \
+		$(TARGET_DIR)/etc/init.d/S50ouistiti
+endef
 
-
-$(eval $(generic-package))
+#$(eval $(kconfig-package))
+$(eval $(autotools-package))
 
