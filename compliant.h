73f7baa8e9a842351d859b1bcb08d9a83b2e3a41
Marc Chalain <marc.chalain@smile.fr>
2018-08-06 15:44:28 +0200

    [build] new C compliance checking

--- compliant.h
+++ compliant.h
@@ -4,18 +4,33 @@
 #ifdef _POSIX_C_SOURCE
 # define HAVE_GETNAMEINFO
 # define HAVE_PWD
+# if _POSIX_C_SOURCE >= 200112L
+#  define HAVE_SYMLINK
+# endif
 # if _POSIX_C_SOURCE >= 199309L
 #  define HAVE_SIGACTION
+# endif
+# if _POSIX_C_SOURCE >= 200809L
+#  define HAVE_STRDUP
+# endif
+# if _POSIX_C_SOURCE >= 2
+#  define HAVE_GETOPT
 # endif
 #endif
 
 #ifdef _XOPEN_SOURCE
-# define HAVE_GETOPTS
+# define HAVE_GETOPT
 # if _XOPEN_SOURCE >= 500
 #  define HAVE_LOCKF
 #  define HAVE_SYMLINK
 #  define HAVE_STRDUP
 # endif
+#endif
+
+#ifdef _BSD_SOURCE
+# define HAVE_LOCKF
+# define HAVE_SYMLINK
+# define HAVE_STRDUP
 #endif
 
 #ifndef NI_MAXHOST
