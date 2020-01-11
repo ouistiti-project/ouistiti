2e48d76f224795e4df30692e5f7e9921d32de6fd
mchalain <marc.chalain@gmail.com>
2020-01-11 10:11:52 +0100

    [build] move mod_methodlock into staging directory

--- staging/mod_methodlock.mk
+++ staging/mod_methodlock.mk
@@ -3,6 +3,8 @@ slib-y+=mod_methodlock
 mod_methodlock_SOURCES-$(METHODLOCK)+=mod_methodlock.c
 mod_methodlock_CFLAGS+=$(LIBHTTPSERVER_CFLAGS)
 mod_methodlock_LDFLAGS+=$(LIBHTTPSERVER_LDFLAGS)
+mod_methodlock_CFLAGS+=-I$(srcdir)src
+mod_methodlock_LDFLAGS+=-L$(srcdir)src
 
 mod_methodlock_CFLAGS-$(DEBUG)+=-g -DDEBUG
 
