a959dbe8aef4bb04a3451dc202714434533671e6
mchalain <marc.chalain@gmail.com>
2020-01-22 17:48:16 +0100

    [document] split putfile_connector code into several functions

--- src/mod_documentrest.c
+++ src/mod_documentrest.c
@@ -54,6 +54,180 @@
 static const char str_OK[] = "OK";
 static const char str_KO[] = "KO";
 
+static int putdir_connector(document_connector_t *private, http_message_t *request, http_message_t *response)
+{
+	int ret =  EREJECT;
+	const char *uri = httpmessage_REQUEST(request,"uri");
+
+	httpmessage_addcontent(response, "text/json", "{\"method\":\"PUT\",\"name\":\"", -1);
+	httpmessage_appendcontent(response, uri, -1);
+	httpmessage_appendcontent(response, "\",\"result\":\"", -1);
+	if (mkdir(private->filepath, 0777) > 0)
+	{
+		err("document: directory creation not allowed %s", private->filepath);
+		httpmessage_appendcontent(response, "KO\"}\n", -1);
+#if defined RESULT_405
+		httpmessage_result(response, RESULT_405);
+#else
+		httpmessage_result(response, RESULT_400);
+#endif
+	}
+	else
+	{
+		warn("document: directory creation %s", private->filepath);
+		httpmessage_appendcontent(response, "OK\"}\n", -1);
+	}
+	ret = ESUCCESS;
+	document_close(private, request);
+	return ret;
+}
+
+static int putheader_connector(document_connector_t *private, http_message_t *request, http_message_t *response)
+{
+	int ret =  EREJECT;
+	const char *uri = httpmessage_REQUEST(request,"uri");
+
+	if (private->size > private->offset)
+	{
+		char range[20];
+		snprintf(range, 20, "bytes %.9ld/*", (long)private->size);
+		httpmessage_addheader(response, "Content-Range", range);
+	}
+	else
+	{
+#ifdef PUTTMPFILE
+#ifndef O_TMPFILE
+#define O_TMPFILE (__O_TMPFILE | O_DIRECTORY)
+#endif
+		/**
+		 * tmpfile will be not visible before to be closed
+		 */
+		char filepath[PATH_MAX];
+		strncpy(filepath, private->filepath, PATH_MAX);
+		char *dirpath = dirname(filepath);
+		private->fd = open(dirpath, O_WRONLY | O_TMPFILE, 0644);
+#else
+		private->fd = open(private->filepath, O_WRONLY | O_CREAT, 0644);
+#endif
+	}
+	if (private->fd > 0)
+	{
+		if (private->offset > 0)
+		{
+			lseek(private->fd, private->offset, SEEK_SET);
+		}
+		ret = EINCOMPLETE;
+		httpmessage_addcontent(response, "text/json", "{\"method\":\"PUT\",\"result\":\"OK\",\"name\":\"", -1);
+		httpmessage_appendcontent(response, uri, -1);
+		httpmessage_appendcontent(response, "\"}\n", -1);
+#ifdef DEBUG
+		clock_gettime(CLOCK_REALTIME, &private->start);
+		dbg("document transfer start: %ld:%ld", private->start.tv_sec, private->start.tv_nsec);
+#endif
+	}
+	else
+	{
+		err("document: file creation not allowed %s (size: %ld)", private->filepath, (long)private->size);
+		httpmessage_addcontent(response, "text/json", "{\"method\":\"PUT\",\"result\":\"KO\",\"name\":\"", -1);
+		httpmessage_appendcontent(response, uri, -1);
+		httpmessage_appendcontent(response, "\"}\n", -1);
+		if (private->size > 0)
+#if defined RESULT_416
+			httpmessage_result(response, RESULT_416);
+#else
+			httpmessage_result(response, RESULT_400);
+#endif
+		else
+#if defined RESULT_405
+			httpmessage_result(response, RESULT_405);
+#else
+			httpmessage_result(response, RESULT_400);
+#endif
+		ret = ESUCCESS;
+		private->fd = 0;
+		document_close(private, request);
+	}
+
+	return ret;
+}
+
+static int putcontent_connector(document_connector_t *private, http_message_t *request, http_message_t *response)
+{
+	int ret =  EREJECT;
+	const char *uri = httpmessage_REQUEST(request,"uri");
+
+	char *input;
+	int inputlen;
+	/**
+	 * rest = 1 to close the connection on end of file or
+	 * on connection error
+	 */
+	unsigned long long rest = 1;
+	inputlen = httpmessage_content(request, &input, &rest);
+
+	/**
+	 * the function returns EINCOMPLETE to wait before to send
+	 * the response.
+	 */
+	ret = EINCOMPLETE;
+	while (inputlen > 0)
+	{
+		int wret = write(private->fd, input, inputlen);
+		if (wret < 0)
+		{
+			err("document: access file %s error %s", private->filepath, strerror(errno));
+			if (errno != EAGAIN)
+			{
+#ifdef RESULT_500
+				httpmessage_result(response, RESULT_500);
+#else
+				httpmessage_result(response, RESULT_404);
+#endif
+				rest = 0;
+				break;
+			}
+		}
+		else if (wret > 0)
+		{
+#ifdef DEBUG
+			private->datasize += wret;
+#endif
+			inputlen -= wret;
+			input += wret;
+		}
+	}
+	if (inputlen == EREJECT)
+	{
+		rest = 0;
+	}
+	if (rest < 1)
+	{
+#ifdef DEBUG
+		struct timespec stop;
+		struct timespec value;
+		clock_gettime(CLOCK_REALTIME, &stop);
+
+		value.tv_sec = stop.tv_sec - private->start.tv_sec;
+		value.tv_nsec = stop.tv_nsec - private->start.tv_nsec;
+		dbg("document: (%llu bytes) time %ld:%03ld", private->datasize, value.tv_sec, value.tv_nsec/1000000);
+#endif
+		warn("document: %s uploaded", private->filepath);
+		if (private->fd)
+		{
+#ifdef PUTTMPFILE
+			char path[PATH_MAX];
+			snprintf(path, PATH_MAX, "/proc/self/fd/%d", private->fd);
+			linkat(AT_FDCWD, path, AT_FDCWD, private->filepath,AT_SYMLINK_FOLLOW);
+#endif
+			close(private->fd);
+		}
+		private->fd = 0;
+		ret = ESUCCESS;
+		document_close(private, request);
+	}
+	return ret;
+}
+
 int putfile_connector(void *arg, http_message_t *request, http_message_t *response)
 {
 	int ret =  EREJECT;
@@ -67,90 +241,11 @@ int putfile_connector(void *arg, http_message_t *request, http_message_t *respon
 		int length = strlen(private->filepath);
 		if (private->filepath[length - 1] == '/')
 		{
-			httpmessage_addcontent(response, "text/json", "{\"method\":\"PUT\",\"name\":\"", -1);
-			httpmessage_appendcontent(response, uri, -1);
-			httpmessage_appendcontent(response, "\",\"result\":\"", -1);
-			if (mkdir(private->filepath, 0777) > 0)
-			{
-				err("document: directory creation not allowed %s", private->filepath);
-				httpmessage_appendcontent(response, "KO\"}\n", -1);
-#if defined RESULT_405
-				httpmessage_result(response, RESULT_405);
-#else
-				httpmessage_result(response, RESULT_400);
-#endif
-			}
-			else
-			{
-				warn("document: directory creation %s", private->filepath);
-				httpmessage_appendcontent(response, "OK\"}\n", -1);
-			}
-			ret = ESUCCESS;
-			document_close(private, request);
-
+			ret = putdir_connector(private, request, response);
 		}
 		else
 		{
-			if (private->size > private->offset)
-			{
-				char range[20];
-				snprintf(range, 20, "bytes %.9ld/*", (long)private->size);
-				httpmessage_addheader(response, "Content-Range", range);
-			}
-			else
-			{
-#ifdef PUTTMPFILE
-#ifndef O_TMPFILE
-#define O_TMPFILE (__O_TMPFILE | O_DIRECTORY)
-#endif
-				/**
-				 * tmpfile will be not visible before to be closed
-				 */
-				char filepath[PATH_MAX];
-				strncpy(filepath, private->filepath, PATH_MAX);
-				char *dirpath = dirname(filepath);
-				private->fd = open(dirpath, O_WRONLY | O_TMPFILE, 0644);
-#else
-				private->fd = open(private->filepath, O_WRONLY | O_CREAT, 0644);
-#endif
-			}
-			if (private->fd > 0)
-			{
-				if (private->offset > 0)
-				{
-					lseek(private->fd, private->offset, SEEK_SET);
-				}
-				ret = EINCOMPLETE;
-				httpmessage_addcontent(response, "text/json", "{\"method\":\"PUT\",\"result\":\"OK\",\"name\":\"", -1);
-				httpmessage_appendcontent(response, uri, -1);
-				httpmessage_appendcontent(response, "\"}\n", -1);
-#ifdef DEBUG
-				clock_gettime(CLOCK_REALTIME, &private->start);
-				dbg("document transfer start: %ld:%ld", private->start.tv_sec, private->start.tv_nsec);
-#endif
-			}
-			else
-			{
-				err("document: file creation not allowed %s (size: %ld)", private->filepath, (long)private->size);
-				httpmessage_addcontent(response, "text/json", "{\"method\":\"PUT\",\"result\":\"KO\",\"name\":\"", -1);
-				httpmessage_appendcontent(response, uri, -1);
-				httpmessage_appendcontent(response, "\"}\n", -1);
-				if (private->size > 0)
-#if defined RESULT_416
-					httpmessage_result(response, RESULT_416);
-#else
-					httpmessage_result(response, RESULT_400);
-#endif
-				else
-#if defined RESULT_405
-					httpmessage_result(response, RESULT_405);
-#else
-					httpmessage_result(response, RESULT_400);
-#endif
-				ret = ESUCCESS;
-				private->fd = 0;
-				document_close(private, request);
-			}
+			ret = putheader_connector(private, request, response);
 		}
 	}
 	/**
@@ -159,75 +254,7 @@ int putfile_connector(void *arg, http_message_t *request, http_message_t *respon
 	 */
 	else if (private->fd > 0)
 	{
-		char *input;
-		int inputlen;
-		/**
-		 * rest = 1 to close the connection on end of file or
-		 * on connection error
-		 */
-		unsigned long long rest = 1;
-		inputlen = httpmessage_content(request, &input, &rest);
-
-		/**
-		 * the function returns EINCOMPLETE to wait before to send
-		 * the response.
-		 */
-		ret = EINCOMPLETE;
-		while (inputlen > 0)
-		{
-			int wret = write(private->fd, input, inputlen);
-			if (wret < 0)
-			{
-				err("document: access file %s error %s", private->filepath, strerror(errno));
-				if (errno != EAGAIN)
-				{
-#ifdef RESULT_500
-					httpmessage_result(response, RESULT_500);
-#else
-					httpmessage_result(response, RESULT_404);
-#endif
-					rest = 0;
-					break;
-				}
-			}
-			else if (wret > 0)
-			{
-#ifdef DEBUG
-				private->datasize += wret;
-#endif
-				inputlen -= wret;
-				input += wret;
-			}
-		}
-		if (inputlen == EREJECT)
-		{
-			rest = 0;
-		}
-		if (rest < 1)
-		{
-#ifdef DEBUG
-			struct timespec stop;
-			struct timespec value;
-			clock_gettime(CLOCK_REALTIME, &stop);
-
-			value.tv_sec = stop.tv_sec - private->start.tv_sec;
-			value.tv_nsec = stop.tv_nsec - private->start.tv_nsec;
-			dbg("document: (%llu bytes) time %ld:%03ld", private->datasize, value.tv_sec, value.tv_nsec/1000000);
-#endif
-			warn("document: %s uploaded", private->filepath);
-			if (private->fd)
-			{
-#ifdef PUTTMPFILE
-				char path[PATH_MAX];
-				snprintf(path, PATH_MAX, "/proc/self/fd/%d", private->fd);
-				linkat(AT_FDCWD, path, AT_FDCWD, private->filepath,AT_SYMLINK_FOLLOW);
-#endif
-				close(private->fd);
-			}
-			private->fd = 0;
-			ret = ESUCCESS;
-			document_close(private, request);
-		}
+		ret = putcontent_connector(private, request, response);
 	}
 	return ret;
 }
