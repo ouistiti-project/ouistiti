f2d0e0080e7aec187064413a613eb1cc367a33cc
mchalain <marc.chalain@smile.fr>
2020-02-19 08:20:30 +0100

    [build] move modules from libhttpserver

---
+++ staging/mod_date.mk
@@ -0,0 +1,7 @@
+modules-$(MODULES)+=mod_date
+slib-y+=mod_date
+mod_date_SOURCES+=mod_date.c
+mod_date_CFLAGS+=$(LIBHTTPSERVER_CFLAGS)
+mod_date_LDFLAGS+=$(LIBHTTPSERVER_LDFLAGS)
+
+mod_date_CFLAGS-$(DEBUG)+=-g -DDEBUG
