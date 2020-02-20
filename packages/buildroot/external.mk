345cd2d01c3f0bff73deeabe192cda720c4b6d4e
Marc Chalain <marc.chalain@gmail.com>
2020-02-20 15:42:10 +0100

    [package] update buildroot scripts

--- packages/buildroot/external.mk
+++ packages/buildroot/external.mk
@@ -4,6 +4,5 @@
 #
 ################################################################################
 
-include $(sort $(wildcard $(BR2_EXTERNAL_OUISTITI_PATH)/libhttpserver/libhttpserver.mk))
-include $(sort $(wildcard $(BR2_EXTERNAL_OUISTITI_PATH)/ouistiti/ouistiti.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_OUISTITI_PATH)/package/*/*.mk))
 
