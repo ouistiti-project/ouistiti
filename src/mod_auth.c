df40672b2d8b83e19abb25914d11de677a29badf
Marc Chalain <marc.chalain@gmail.com>
2020-09-02 08:12:13 +0200

    [auth] move auth management from sqlite to mod

--- src/mod_auth.c
+++ src/mod_auth.c
@@ -85,6 +85,7 @@ static void *_mod_auth_getctx(void *arg, http_client_t *ctl, struct sockaddr *ad
 static void _mod_auth_freectx(void *vctx);
 static int _home_connector(void *arg, http_message_t *request, http_message_t *response);
 static int _authn_connector(void *arg, http_message_t *request, http_message_t *response);
+static int _authz_connector(void *arg, http_message_t *request, http_message_t *response);
 #ifndef AUTHZ_JWT
 static char *authz_generatetoken(mod_auth_t *mod, authsession_t *info);
 #endif
@@ -113,6 +114,13 @@ struct _mod_auth_s
 const char str_authenticate[] = "WWW-Authenticate";
 const char str_authorization[] = "Authorization";
 const char str_anonymous[] = "anonymous";
+
+#ifdef AUTHZ_MANAGER
+static const char str_put[] = "PUT";
+static const char str_delete[] = "DELETE";
+
+static const char str_authmngt[] = "/authmngt";
+#endif
 
 static const char *str_xtoken = "X-Auth-Token";
 static const char *str_xuser = "X-Remote-User";
@@ -302,6 +310,10 @@ void *mod_auth_create(http_server_t *server, mod_auth_t *config)
 		mod->type = config->authn.name;
 		mod->typelength = strlen(mod->type);
 		httpserver_addmod(server, _mod_auth_getctx, _mod_auth_freectx, mod, str_auth);
+#ifdef AUTHZ_MANAGER
+		httpserver_addmethod(server, str_put, MESSAGE_ALLOW_CONTENT);
+		httpserver_addmethod(server, str_delete, MESSAGE_ALLOW_CONTENT);
+#endif
 	}
 	else
 	{
@@ -348,6 +360,9 @@ static void *_mod_auth_getctx(void *arg, http_client_t *ctl, struct sockaddr *ad
 	if (mod->authz->type & AUTHZ_HOME_E)
 		httpclient_addconnector(ctl, _home_connector, ctx, CONNECTOR_AUTH, str_auth);
 	httpclient_addconnector(ctl, _authn_connector, ctx, CONNECTOR_AUTH, str_auth);
+#ifdef AUTHZ_MANAGER
+	httpclient_addconnector(ctl, _authz_connector, ctx, CONNECTOR_DOCUMENT, "authz");
+#endif
 	/**
 	 * authn may require prioritary connector and it has to be added after this one
 	 */
@@ -860,6 +875,127 @@ static int _authn_checkuri(const mod_auth_t *config, http_message_t *request, ht
 
 	return ret;
 }
+
+#ifdef AUTHZ_MANAGER
+
+static int _authz_connector(void *arg, http_message_t *request, http_message_t *response)
+{
+	int ret = EREJECT;
+	_mod_auth_ctx_t *ctx = (_mod_auth_ctx_t *)arg;
+	_mod_auth_t *mod = ctx->mod;
+
+	const char *uri = httpmessage_REQUEST(request, "uri");
+	const char *method = httpmessage_REQUEST(request, "method");
+	if (!strcmp(uri, str_authmngt))
+	{
+		const char *user = NULL;
+		const char *group = NULL;
+		const char *home = NULL;
+		const char *token = NULL;
+		const char *passwd = NULL;
+
+		authsession_t session;
+		memset(&session, 0, sizeof(session));
+		const char *query = httpmessage_REQUEST(request, "query");
+
+		char *storage = strdup(query);
+		user = strstr(storage, "user=");
+		group = strstr(storage, "group=");
+		home = strstr(storage, "home=");
+		passwd = strstr(storage, "password=");
+		if (user != NULL)
+		{
+			user += 5;
+			char *end = strchr(user, '&');
+			if (end != NULL)
+				*end = '\0';
+		}
+
+		if (!strcmp(method, str_get))
+		{
+			ret = ESUCCESS;
+			if (user == NULL)
+				user = auth_info(request, "user");
+		}
+		else
+		{
+			int add_passwd = 0;
+
+			if (user != NULL)
+			{
+				session.user = (char *)user;
+			}
+			if (group != NULL)
+			{
+				group += 6;
+				char *end = strchr(group, '&');
+				if (end != NULL)
+					*end = '\0';
+				session.group = (char *)group;
+			}
+			if (home != NULL)
+			{
+				home += 5;
+				char *end = strchr(home, '&');
+				if (end != NULL)
+					*end = '\0';
+				session.home = (char *)home;
+			}
+			if (passwd != NULL)
+			{
+				passwd += 9;
+				char *end = strchr(passwd, '&');
+				if (end != NULL)
+					*end = '\0';
+				session.passwd = (char *)passwd;
+			}
+
+			if (!strcmp(method, str_put) && session.user && session.group)
+			{
+				if ((mod->authz->rules->adduser != NULL) &&
+					(ret = mod->authz->rules->adduser(mod->authz->ctx, &session)) == ESUCCESS)
+					add_passwd = 1;
+			}
+			if ((add_passwd || !strcmp(method, str_post)) && session.user && session.passwd)
+			{
+				if (mod->authz->rules->changepasswd != NULL)
+					ret = mod->authz->rules->changepasswd(mod->authz->ctx, &session);
+			}
+			if (!strcmp(method, str_delete) && session.user)
+			{
+				if (mod->authz->rules->removeuser != NULL)
+					ret = mod->authz->rules->removeuser(mod->authz->ctx, &session);
+			}
+		}
+		if (ret != ESUCCESS)
+		{
+			httpmessage_result(response, RESULT_500);
+			ret = ESUCCESS;
+		}
+		else if (user != NULL)
+		{
+			httpmessage_addcontent(response, "application/x-www-form-urlencoded",
+				"user=", -1);
+			httpmessage_appendcontent(response, user, -1);
+			group = mod->authz->rules->group(mod->authz->ctx, user);
+			if (group != NULL)
+			{
+				httpmessage_appendcontent(response, "&group=", -1);
+				httpmessage_appendcontent(response, group, -1);
+			}
+			if (token != NULL)
+			{
+				httpmessage_appendcontent(response, "&token=", -1);
+				httpmessage_appendcontent(response, token, -1);
+			}
+			httpmessage_appendcontent(response, "\n", -1);
+		}
+		free(storage);
+	}
+	return ret;
+}
+
+#endif
 
 static int _authn_connector(void *arg, http_message_t *request, http_message_t *response)
 {
