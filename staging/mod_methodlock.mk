b77b9409be45f53378895a99a59c5d69b8ed5427
Marc Chalain <marc.chalain@smile.fr>
2021-03-18 14:33:52 +0100

    [methodlock] module DEPRECATED

--- staging/mod_methodlock.mk
+++ staging/mod_methodlock.mk
@@ -1,6 +1,6 @@
 modules-$(MODULES)+=mod_methodlock
 slib-y+=mod_methodlock
-mod_methodlock_SOURCES-$(METHODLOCK)+=mod_methodlock.c
+mod_methodlock_SOURCES+=mod_methodlock.c
 mod_methodlock_CFLAGS+=$(LIBHTTPSERVER_CFLAGS)
 mod_methodlock_LDFLAGS+=$(LIBHTTPSERVER_LDFLAGS)
 mod_methodlock_CFLAGS+=-I$(srcdir)src
