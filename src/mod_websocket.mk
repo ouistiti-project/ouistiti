0ecb7bbc354b9632bf73127b2178ee2d9f530364
Marc Chalain <marc.chalain@gmail.com>
2021-04-16 13:28:03 +0200

    [build] libwebsocket is renamed libouibsocket

--- src/mod_websocket.mk
+++ src/mod_websocket.mk
@@ -7,7 +7,7 @@ mod_websocket_LDFLAGS+=$(LIBHTTPSERVER_LDFLAGS)
 mod_websocket_SOURCES-$(WEBSOCKET)+=mod_websocket.c
 mod_websocket_LDFLAGS+=-L../staging
 mod_websocket_LIBS-$(WEBSOCKET_RT)+=websocket_clirt
-mod_websocket_LIBS+=websocket
+mod_websocket_LIBS+=ouibsocket
 mod_websocket_LIBS+=ouihash
 
 mod_websocket_CFLAGS-$(DEBUG)+=-g -DDEBUG
