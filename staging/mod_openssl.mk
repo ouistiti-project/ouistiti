5ad0ac5c582ce4540772995c089c11ccca0370d9
Marc Chalain <marc.chalain@gmail.com>
2021-09-07 14:38:03 +0200

    [ssl] fix headers path for OpenSSL

--- staging/mod_openssl.mk
+++ staging/mod_openssl.mk
@@ -3,6 +3,7 @@ slib-$(STATIC)+=mod_openssl
 mod_openssl_SOURCES+=mod_openssl.c
 mod_openssl_LIBRARY+=ssl crypto
 mod_openssl_CFLAGS+=$(LIBHTTPSERVER_CFLAGS)
+mod_openssl_CFLAGS+=-I../src
 mod_openssl_LDFLAGS+=$(LIBHTTPSERVER_LDFLAGS)
 mod_openssl_ALIAS-$(MODULES)+=mod_tls.so
 
