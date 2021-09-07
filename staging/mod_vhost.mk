aed0e1df5541d4fbe90e1e5c1d5df926aae0be3e
Marc Chalain <marc.chalain@gmail.com>
2021-09-07 14:38:58 +0200

    [vhost] fix missing headers path

--- staging/mod_vhost.mk
+++ staging/mod_vhost.mk
@@ -3,6 +3,7 @@ slib-$(STATIC)+=mod_vhost
 mod_vhost_SOURCES+=mod_vhost.c
 mod_vhost_CFLAGS+=$(LIBHTTPSERVER_CFLAGS)
 mod_vhost_LDFLAGS+=$(LIBHTTPSERVER_LDFLAGS)
+mod_vhost_CFLAGS+=-I$(srcdir)include
 mod_vhost_CFLAGS+=-I$(srcdir)src
 mod_vhost_LDFLAGS+=-L$(srcdir)src
 
