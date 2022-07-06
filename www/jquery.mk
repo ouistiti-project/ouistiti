7eaf3bc67119eb0b7c7fbdb7a86eccd0ec088b3f
mchalain <marc.chalain@gmail.com>
2022-07-06 16:03:34 +0200

    [www] add webutils to create simple website

---
+++ www/jquery.mk
@@ -0,0 +1,14 @@
+JQUERY_VERSION=3.6.0
+JQUERY_DOCS?=/srv/www/htdocs
+JQUERY_FILES+=$(JQUERY_DOCS)/js/jquery-$(JQUERY_VERSION).min.js
+
+download-y+=jquery
+jquery_SITE?=https://code.jquery.com
+jquery_SOURCE=jquery-$(JQUERY_VERSION).min.js
+
+data-y+=$(JQUERY_FILES)
+
+$(JQUERY_FILES): $(JQUERY_DOCS)/js/%:
+	@mv -f $* $@
+
+$(JQUERY_DOCS)/js/jquery-$(JQUERY_VERSION).min.js_ALIAS+=jquery.min.js
