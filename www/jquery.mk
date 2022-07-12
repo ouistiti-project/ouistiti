2e6c30d23f44005c6c9eb40f1e554174ea2ae1f8
mchalain <marc.chalain@gmail.com>
2022-07-12 14:57:32 +0200

    [www] fix data split on jsonrpc receiving

--- www/jquery.mk
+++ www/jquery.mk
@@ -1,5 +1,5 @@
 JQUERY_VERSION=3.6.0
-JQUERY_DOCS?=/srv/www/htdocs
+JQUERY_DOCS?=srv/www/htdocs
 JQUERY_FILES+=$(JQUERY_DOCS)/js/jquery-$(JQUERY_VERSION).min.js
 
 download-y+=jquery
@@ -9,6 +9,7 @@ jquery_SOURCE=jquery-$(JQUERY_VERSION).min.js
 data-y+=$(JQUERY_FILES)
 
 $(JQUERY_FILES): $(JQUERY_DOCS)/js/%:
-	@mv -f $* $@
+	$(Q)mkdir -p $(@D)
+	$(Q)mv -f $* $@
 
 $(JQUERY_DOCS)/js/jquery-$(JQUERY_VERSION).min.js_ALIAS+=jquery.min.js
