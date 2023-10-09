08129e92ce51b5d82c11faf4ae22d4858c0a7237
mchalain <marc.chalain@scantech.com>
2023-10-09 09:11:15 +0200

    cgi: use string_t for target field

--- src/cgi_env.c
+++ src/cgi_env.c
@@ -34,6 +34,7 @@
 
 #include "ouistiti/httpserver.h"
 #include "ouistiti/log.h"
+#include "ouistiti.h"
 #include "mod_cgi.h"
 #include "mod_auth.h"
 
@@ -47,7 +48,7 @@ typedef const char *(*httpenv_callback_t)(const mod_cgi_config_t *config, http_m
 struct httpenv_s
 {
 	int id;
-	char *target;
+	string_t target;
 	int length;
 	int options;
 	httpenv_callback_t cb;
@@ -209,217 +210,217 @@ static const httpenv_t cgi_env[] =
 {
 	{
 		.id = -1,
-		.target = "CONTENT_LENGTH=",
+		.target = STRING_DCL("CONTENT_LENGTH="),
 		.length = 16,
 		.cb = &env_requestcontentlength,
 	},
 	{
 		.id = -1,
-		.target = "GATEWAY_INTERFACE=",
+		.target = STRING_DCL("GATEWAY_INTERFACE="),
 		.length = 26,
 		.cb = &env_gatewayinterface,
 	},
 	{
 		.id = PATH_INFO,
-		.target = "PATH_INFO=",
+		.target = STRING_DCL("PATH_INFO="),
 		.length = 512,
 	},
 	{
 		.id = PATH_INFO,
-		.target = "PATH_TRANSLATED=",
+		.target = STRING_DCL("PATH_TRANSLATED="),
 		.length = 512,
 	},
 	{
 		.id = SCRIPT_FILENAME,
-		.target = "SCRIPT_FILENAME=",
+		.target = STRING_DCL("SCRIPT_FILENAME="),
 		.length = 512,
 	},
 	{
 		. id = SCRIPT_NAME,
-		.target = "SCRIPT_NAME=",
+		.target = STRING_DCL("SCRIPT_NAME="),
 		.length = 64,
 	},
 	{
 		.id = -1,
-		.target = "DOCUMENT_ROOT=",
+		.target = STRING_DCL("DOCUMENT_ROOT="),
 		.length = 512,
 		.cb = &env_docroot,
 	},
 	{
 		.id = -1,
-		.target = "SERVER_SOFTWARE=",
+		.target = STRING_DCL("SERVER_SOFTWARE="),
 		.length = 26,
 		.cb = &env_serversoftware,
 
 	},
 	{
 		.id = -1,
-		.target = "SERVER_NAME=",
+		.target = STRING_DCL("SERVER_NAME="),
 		.length = 26,
 		.cb = &env_servername,
 	},
 	{
 		.id = -1,
-		.target = "SERVER_PROTOCOL=",
+		.target = STRING_DCL("SERVER_PROTOCOL="),
 		.length = 10,
 		.cb = &env_serverprotocol,
 	},
 	{
 		.id = -1,
-		.target = "SERVER_ADDR=",
+		.target = STRING_DCL("SERVER_ADDR="),
 		.length = 26,
 		.cb = &env_serveraddr,
 	},
 	{
 		.id = -1,
-		.target = "SERVER_PORT=",
+		.target = STRING_DCL("SERVER_PORT="),
 		.length = 6,
 		.cb = &env_serverport,
 	},
 	{
 		.id = -1,
-		.target = "SERVER_SERVICE=",
+		.target = STRING_DCL("SERVER_SERVICE="),
 		.length = 26,
 		.cb = &env_serverservice,
 	},
 	{
 		.id = -1,
-		.target = "REQUEST_METHOD=",
+		.target = STRING_DCL("REQUEST_METHOD="),
 		.length = 6,
 		.cb = &env_requestmethod,
 	},
 	{
 		.id = -1,
-		.target = "REQUEST_SCHEME=",
+		.target = STRING_DCL("REQUEST_SCHEME="),
 		.length = 6,
 		.cb = &env_requestscheme,
 	},
 	{
 		.id = -1,
-		.target = "REQUEST_URI=",
+		.target = STRING_DCL("REQUEST_URI="),
 		.length = 512,
 		.cb = &env_requesturi,
 	},
 	{
 		.id = -1,
-		.target = "CONTENT_TYPE=",
+		.target = STRING_DCL("CONTENT_TYPE="),
 		.length = 128,
 		.cb = &env_requestcontenttype,
 	},
 	{
 		.id = -1,
-		.target = "QUERY_STRING=",
+		.target = STRING_DCL("QUERY_STRING="),
 		.length = 512,
 		.cb = &env_requestquery,
 	},
 	{
 		.id = -1,
-		.target = "HTTP_ACCEPT=",
+		.target = STRING_DCL("HTTP_ACCEPT="),
 		.length = 128,
 		.options = ENV_NOTREQUIRED,
 		.cb = &env_requestaccept,
 	},
 	{
 		.id = -1,
-		.target = "HTTP_ACCEPT_ENCODING=",
+		.target = STRING_DCL("HTTP_ACCEPT_ENCODING="),
 		.length = 128,
 		.options = ENV_NOTREQUIRED,
 		.cb = &env_requestacceptencoding,
 	},
 	{
 		.id = -1,
-		.target = "HTTP_ACCEPT_LANGUAGE=",
+		.target = STRING_DCL("HTTP_ACCEPT_LANGUAGE="),
 		.length = 64,
 		.options = ENV_NOTREQUIRED,
 		.cb = &env_requestacceptlanguage,
 	},
 	{
 		.id = -1,
-		.target = "REMOTE_HOST=",
+		.target = STRING_DCL("REMOTE_HOST="),
 		.length = 26,
 		.cb = &env_remotehost,
 	},
 	{
 		.id = -1,
-		.target = "REMOTE_ADDR=",
+		.target = STRING_DCL("REMOTE_ADDR="),
 		.length = INET6_ADDRSTRLEN,
 		.cb = &env_remoteaddr,
 	},
 	{
 		.id = -1,
-		.target = "REMOTE_PORT=",
+		.target = STRING_DCL("REMOTE_PORT="),
 		.length = 26,
 		.cb = &env_remoteport,
 	},
 	{
 		.id = -1,
-		.target = "REMOTE_USER=",
+		.target = STRING_DCL("REMOTE_USER="),
 		.length = 26,
 		.options = ENV_NOTREQUIRED,
 		.cb = &env_authuser,
 	},
 	{
 		.id = -1,
-		.target = "AUTH_TYPE=",
+		.target = STRING_DCL("AUTH_TYPE="),
 		.length = 26,
 		.options = ENV_NOTREQUIRED,
 		.cb = &env_authtype,
 	},
 	{
 		.id = -1,
-		.target = "HTTP_COOKIE=",
+		.target = STRING_DCL("HTTP_COOKIE="),
 		.length = 512,
 		.cb = &env_requestcookie,
 	},
 	{
 		.id = -1,
-		.target = "HTTP_HOST=",
+		.target = STRING_DCL("HTTP_HOST="),
 		.length = 512,
 		.cb = &env_requesthost,
 	},
 	{
 		.id = -1,
-		.target = "HTTP_USER_AGENT=",
+		.target = STRING_DCL("HTTP_USER_AGENT="),
 		.length = 512,
 		.cb = &env_requestuseragent,
 	},
 	{
 		.id = -1,
-		.target = "HTTP_REFERER=",
+		.target = STRING_DCL("HTTP_REFERER="),
 		.length = 512,
 		.options = ENV_NOTREQUIRED,
 		.cb = &env_requestreferer,
 	},
 	{
 		.id = -1,
-		.target = "HTTP_ORIGIN=",
+		.target = STRING_DCL("HTTP_ORIGIN="),
 		.length = 512,
 		.options = ENV_NOTREQUIRED,
 		.cb = &env_requestorigin,
 	},
 	{
 		.id = -1,
-		.target = "AUTH_USER=",
+		.target = STRING_DCL("AUTH_USER="),
 		.length = 26,
 		.options = ENV_NOTREQUIRED,
 		.cb = &env_authuser,
 	},
 	{
 		.id = HTTPS,
-		.target = "HTTPS=",
+		.target = STRING_DCL("HTTPS="),
 		.length = 1,
 		.options = ENV_NOTREQUIRED,
 	},
 	{
 		.id = -1,
-		.target = "REMOTE_GROUP=",
+		.target = STRING_DCL("REMOTE_GROUP="),
 		.length = 26,
 		.options = ENV_NOTREQUIRED,
 		.cb = &env_authgroup,
 	},
 	{
 		.id = -1,
-		.target = "AUTH_GROUP=",
+		.target = STRING_DCL("AUTH_GROUP="),
 		.length = 26,
 		.options = ENV_NOTREQUIRED,
 		.cb = &env_authgroup,
@@ -438,7 +439,7 @@ char **cgi_buildenv(const mod_cgi_config_t *config, http_message_t *request, con
 	for (i = 0; i < nbenvs; i++)
 	{
 		int options = cgi_env[i].options;
-		size_t length = strlen(cgi_env[i].target) + cgi_env[i].length;
+		size_t length = cgi_env[i].target.length + cgi_env[i].length;
 		env[i] = (char *)calloc(1, length + 1);
 		const char *value = NULL;
 		int valuelength = -1;
@@ -472,7 +473,7 @@ char **cgi_buildenv(const mod_cgi_config_t *config, http_message_t *request, con
 		{
 			if (valuelength == -1)
 				valuelength = (int)strlen(value);
-			snprintf(env[j], length + 1, "%s%.*s", cgi_env[i].target, valuelength, value);
+			snprintf(env[j], length + 1, "%s%.*s", cgi_env[i].target.data, valuelength, value);
 			j++;
 		}
 	}
