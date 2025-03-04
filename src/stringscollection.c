045cf7e6bd47fab421741fa101123c4b1ba3abbb
mchalain <marc.chalain@scantech.com>
2025-03-04 09:56:14 +0100

    auth: manage several issuers inside the same session

--- src/stringscollection.c
+++ src/stringscollection.c
@@ -69,6 +69,7 @@ const char str_user[] = "user";
 const char str_group[] = "group";
 const char str_home[] = "home";
 const char str_status[] = "status";
+const char str_issuer[] = "issuer";
 
 const char str_status_approving[] = "approving";
 const char str_status_reapproving[] = "reapproving";
