a970b3d989807ab2fd76c30a083c3d49d06a7b6c
Marc Chalain <marc.chalain@gmail.com>
2025-04-21 08:44:11 +0200

    redirect: send 204 if redirect_uri is empty

--- src/mod_redirect.c
+++ src/mod_redirect.c
@@ -341,15 +341,19 @@ static int _mod_redirect_connectorlink(_mod_redirect_t *mod, http_message_t *req
 		}
 		else if (link->options & REDIRECT_QUERY)
 		{
-			const char *redirect = NULL;
-			size_t length = httpmessage_parameter(request, "redirect_uri", &redirect);
-			char *decode = utils_urldecode(redirect, length);
+			string_t redirect = {0};
+			ouimessage_parameter(request, "redirect_uri", &redirect);
+			char *decode = NULL;
+			if (!string_empty(&redirect))
+				decode = utils_urldecode(string_toc(&redirect), string_length(&redirect));
 			if (decode != NULL)
 			{
 				redirect_dbg("redirect: Location from query %s", decode);
 				httpmessage_addheader(response, str_location, decode, strlen(decode));
 				free(decode);
 			}
+			else
+				result = RESULT_204;
 			ret = ESUCCESS;
 		}
 		if (ret == ESUCCESS)
