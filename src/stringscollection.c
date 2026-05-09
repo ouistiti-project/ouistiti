fad9075fb202acebf8551d60e2108620bcbabae0
Marc Chalain <marc.chalain@gmail.com>
2026-05-10 00:14:44 +0200

    string: new "slash" string

--- src/stringscollection.c
+++ src/stringscollection.c
@@ -91,3 +91,4 @@ const string_t string_xgroup = STRING_DCL(str_xgroup);
 const string_t string_xhome = STRING_DCL(str_xhome);
 
 const string_t string_dot = STRING_DCL(".");
+const string_t string_slash = STRING_DCL("/");
